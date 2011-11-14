d73e89822dee0737ec564109aacf920a2d9cf8cb
Tino Didriksen <mail@tinodidriksen.com>
2011-11-14 15:27:42 +0000

    LG2
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/external/LearnGreenlandic2@7689 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- src/LecturePlayer.cpp
+++ src/LecturePlayer.cpp
@@ -1,5 +1,7 @@
-#include "CryptFile.hpp"
 #include "LecturePlayer.hpp"
+#include <QtGlobal>
+#if defined(Q_WS_WIN)
+
 #include <QProgressDialog>
 #include <QCryptographicHash>
 #include <iostream>
@@ -31,13 +33,6 @@ tc(tc)
 
     video = new QAxWidget("{6BF52A52-394A-11D3-B153-00C04F79FAA6}");
     controls = video->querySubObject("controls");
-    /*
-    QString s = controls->generateDocumentation();
-    QFile f("C:/Temp/IWMPControls.html");
-    f.open(QIODevice::WriteOnly);
-    f.write(s.toUtf8());
-    f.close();
-    //*/
 
     QCryptographicHash sha1(QCryptographicHash::Sha1);
     sha1.addData(lecfile.toUtf8());
@@ -150,3 +145,157 @@ QSize LecturePlayer::sizeHint() const {
 QSize LecturePlayer::minimumSizeHint() const {
     return QSize(1010, 335);
 }
+
+#else
+
+LecturePlayer::LecturePlayer(TaskChooser& tc, QString lecDir, QString title) :
+QWidget(0, Qt::Window | Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint),
+tc(tc)
+{
+    setWindowTitle(title);
+    setMinimumSize(minimumSizeHint());
+
+    QString lecfile = find_newest(tc.dirs, lecDir + "/lecture.dat");
+    if (lecfile.isEmpty()) {
+        QMessageBox::critical(0, "Missing Lecture Data!", "Could not locate lecture.dat!");
+        throw(-1);
+    }
+
+    QString slide0 = find_newest(tc.dirs, lecDir + "/0.png");
+    if (slide0.isEmpty()) {
+        QMessageBox::critical(0, "Missing Slides!", "Could not locate slides for lecture!");
+        throw(-1);
+    }
+
+    QFileInfoList slideFiles = QDir(QFileInfo(slide0).absoluteDir()).entryInfoList(QStringList() << "*.png");
+    foreach (QFileInfo slideFile, slideFiles) {
+        uint32_t key = slideFile.baseName().toULong();
+        slides[key] = slideFile.canonicalFilePath();
+    }
+
+    media = new Phonon::MediaObject;
+    video = new Phonon::VideoWidget;
+    Phonon::createPath(media, video);
+
+    audio = new Phonon::AudioOutput(Phonon::VideoCategory);
+    Phonon::createPath(media, audio);
+
+    mediafile = new CryptFile(lecfile);
+    media->setCurrentSource(mediafile);
+    media->setTickInterval(1000);
+    connect(media, SIGNAL(tick(qint64)), this, SLOT(tick(qint64)));
+    connect(media, SIGNAL(finished()), this, SLOT(finished()));
+
+    video->setAspectRatio(Phonon::VideoWidget::AspectRatio16_9);
+    video->setMinimumSize(400, 225);
+    video->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+    video->setContentsMargins(0, 0, 0, 0);
+
+    QPixmap qpx;
+    curSlide = slides.begin().value();
+    qpx.load(curSlide);
+
+    scene = new QGraphicsScene;
+    scene->setBackgroundBrush(QBrush(QColor(0, 51, 102), Qt::SolidPattern));
+    slide = scene->addPixmap(qpx);
+    slide->setTransformationMode(Qt::SmoothTransformation);
+
+    view = new ScalingGraphicsView(scene);
+    view->setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setContentsMargins(0, 0, 0, 0);
+
+    QGridLayout *qgl = new QGridLayout;
+    qgl->addWidget(video, 0, 0);
+    qgl->addWidget(view, 0, 1);
+    qgl->setColumnStretch(0, 545);
+    qgl->setColumnStretch(1, 455);
+    qgl->setSpacing(2);
+    qgl->setContentsMargins(0, 0, 0, 0);
+
+    playpause = new QPushButton(style()->standardIcon(QStyle::SP_MediaPause), "Pause", this);
+    playpause->setShortcut(QString("Space"));
+    connect(playpause, SIGNAL(clicked()), this, SLOT(togglePlay()));
+
+    seeker = new Phonon::SeekSlider(this);
+    seeker->setMediaObject(media);
+
+    QPalette palette;
+    palette.setBrush(QPalette::Light, Qt::darkGray);
+
+    timeLcd = new QLCDNumber;
+    timeLcd->setPalette(palette);
+
+    QHBoxLayout *qhbl = new QHBoxLayout;
+    qhbl->addWidget(playpause, 1);
+    qhbl->addWidget(seeker, 96);
+    qhbl->addWidget(timeLcd, 1);
+
+    qgl->addLayout(qhbl, 1, 0, 1, 2);
+
+    setLayout(qgl);
+    setContentsMargins(0, 0, 0, 0);
+}
+
+void LecturePlayer::closeEvent(QCloseEvent *event) {
+    media->stop();
+    media->clear();
+    event->accept();
+}
+
+void LecturePlayer::show() {
+    QWidget::show();
+    media->play();
+}
+
+void LecturePlayer::tick(qint64 time) {
+    QTime displayTime(0, (time / 60000) % 60, (time / 1000) % 60);
+    timeLcd->display(displayTime.toString("mm:ss"));
+
+    uint32_t i = time/1000;
+    QString nSlide = curSlide;
+    slides_t::const_iterator it = slides.find(i);
+    if (it != slides.end()) {
+        nSlide = it.value();
+    }
+    else {
+        it = slides.lowerBound(time/1000);
+        --it;
+        nSlide = it.value();
+    }
+    if (nSlide != curSlide) {
+        curSlide = nSlide;
+        QPixmap qp;
+        qp.load(curSlide);
+        slide->setPixmap(qp);
+        slide->update();
+    }
+}
+
+void LecturePlayer::togglePlay() {
+    if (media->state() == Phonon::PlayingState) {
+        playpause->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+        playpause->setText("Play");
+        media->pause();
+    }
+    else {
+        playpause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+        playpause->setText("Pause");
+        media->play();
+    }
+}
+
+void LecturePlayer::finished() {
+
+}
+
+QSize LecturePlayer::sizeHint() const {
+    return QSize(1010, 335);
+}
+
+QSize LecturePlayer::minimumSizeHint() const {
+    return QSize(1010, 335);
+}
+
+#endif
--- src/LecturePlayer.hpp
+++ src/LecturePlayer.hpp
@@ -3,7 +3,11 @@
 
 #include "common.hpp"
 #include "ScalingGraphicsView.hpp"
+#include "CryptFile.hpp"
 #include "TaskChooser.hpp"
+
+#include <QtGlobal>
+#if defined(Q_WS_WIN)
 
 #include <QAxWidget>
 #include <QAxObject>
@@ -41,4 +45,54 @@ private:
     QString tmpfile;
 };
 
+#else
+
+#include <phonon/mediaobject.h>
+#include <phonon/audiooutput.h>
+#include <phonon/mediasource.h>
+#include <phonon/videowidget.h>
+#include <phonon/seekslider.h>
+#include <phonon/effect.h>
+#include <QtGui>
+#include <stdint.h>
+
+class LecturePlayer : public QWidget {
+    Q_OBJECT
+
+public:
+    LecturePlayer(TaskChooser& tc, QString lecDir, QString title);
+
+    void closeEvent(QCloseEvent *event);
+
+    QSize sizeHint() const;
+    QSize minimumSizeHint() const;
+
+public slots:
+    void show();
+    void tick(qint64 time);
+    void togglePlay();
+    void finished();
+
+private:
+    TaskChooser& tc;
+
+    typedef QMap<uint32_t, QString> slides_t;
+    slides_t slides;
+    QString curSlide;
+
+    QGraphicsScene *scene;
+    ScalingGraphicsView *view;
+    QGraphicsPixmapItem *slide;
+
+    CryptFile *mediafile;
+    Phonon::MediaObject *media;
+    Phonon::VideoWidget *video;
+    Phonon::AudioOutput *audio;
+
+    QPushButton *playpause;
+    Phonon::SeekSlider *seeker;
+    QLCDNumber *timeLcd;
+};
+
+#endif // Q_WS_WIN
 #endif // LECTUREPLAYER_H
