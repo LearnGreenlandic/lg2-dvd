5150145e7a421bccdd26beb4079959e03a70435b
Tino Didriksen <mail@tinodidriksen.com>
2011-11-29 22:58:49 +0000

    LG2
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/external/LearnGreenlandic2@7749 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- scripts/osx-deploy-etc.sh
+++ scripts/osx-deploy-etc.sh
@@ -1,4 +1,4 @@
 rm -rfv "Learn Greenlandic 2.app"
 time macdeployqt LearnGreenlandic2.app -dmg
 mv LearnGreenlandic2.app "Learn Greenlandic 2.app"
-rsync -avzP --delete "Learn Greenlandic 2.app" kal@learngreenlandic.com:public_html/demo2/
+rsync -avzP --delete "Learn Greenlandic 2.app" kal@learngreenlandic.com:files/demo2/
